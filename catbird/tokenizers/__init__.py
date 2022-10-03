14c778e72e4e9f3230d4a612ec7679107a9e5c79
Afonso Sousa <afonsousa2806@gmail.com>
2022-10-03 14:40:50 +0000

    Tokenizer module clean

--- catbird/tokenizers/__init__.py
+++ catbird/tokenizers/__init__.py
@@ -1,7 +1,3 @@
-"""Import facility for 'tokenizers' classes and functions."""
-
 from .builder import build_tokenizer
 
-__all__ = [
-    "build_tokenizer",
-]
+__all__ = ["build_tokenizer"]
--- catbird/tokenizers/builder.py
+++ catbird/tokenizers/builder.py
@@ -1,10 +1,18 @@
 """Factory to build tokenizers."""
-from catbird.core import Config  # type: ignore
+# from urllib.error import HTTPError
+from typing import Any
+
+import ignite.distributed as idist
+from ignite.utils import setup_logger
+from requests import HTTPError
 from transformers import AutoTokenizer
-from urllib.error import HTTPError
+
+from catbird.utils import Config
+
+logger = setup_logger(name="Tokenizer", distributed_rank=idist.get_rank())
 
 
-def build_tokenizer(cfg: Config) -> AutoTokenizer:
+def build_tokenizer(cfg: Config) -> Any:
     """Abstraction to build tokenizers based on the given configurations.
 
     Args:
@@ -17,15 +25,20 @@ def build_tokenizer(cfg: Config) -> AutoTokenizer:
         nn.Module: selected model based on configurations
     """
     try:
-        tokenizer = AutoTokenizer.from_pretrained(cfg.model.name.lower())
+        tokenizer = AutoTokenizer.from_pretrained(cfg.tokenizer.name)
         if cfg.get("tokenizer", None):
             tokenizer.add_special_tokens(cfg.tokenizer.special_tokens)
         return tokenizer
     except HTTPError as err:
-        raise HTTPError(f"Server error from HuggingFace.\nError message:\n{err}")
-    except:
-        print(
-            "The model name does not match an existing tokenizer. Returning bert-base-uncased pretrained tokenizer."
-        )
-        tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-        return tokenizer
+        if err.response.status_code == 404:
+            raise HTTPError(f"Server error from HuggingFace.\nError message:\n{err}")
+        elif err.response.status_code == 401:
+            logger.info(
+                "The model name does not match an existing tokenizer. Returning BPE pretrained tokenizer from Roberta."
+            )
+            tokenizer = AutoTokenizer.from_pretrained("roberta-base")
+            return tokenizer
+        else:
+            raise HTTPError(
+                "Something went wrong for unknown reasons. Please try again later."
+            )
